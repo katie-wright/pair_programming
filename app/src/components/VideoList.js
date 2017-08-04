476cb26d277b1bc723e73946c9d6230a96c294b7
unknown <digitalhome@gmail.com>
2017-08-04 15:24:35 -0400

    video enabled

--- app/src/components/VideoList.js
+++ app/src/components/VideoList.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
+const YouTubeIframeLoader = require('youtube-iframe') ;
 
 class VideoList extends Component {
+
     render(){
         let vlist = this.props.videos.map((video,i)=>{
             return (
                 <Video 
-                    key={video.id}
+                    id={video.id}
                     title={video.snippet.title}
+                    description={video.snippet.description}
+                    user={video.snippet.channelTitle}
+                    views={video.statistics.viewCount}
+                    date={video.snippet.publishedAt}
+                    tags={video.snippet.tags}
+                    category={video.snippet.categoryId}
                     />
             )
         })
@@ -20,10 +28,27 @@ class VideoList extends Component {
 }
 
 class Video extends Component {
+
     render(){
+        let videoId = this.props.id;
+        YouTubeIframeLoader.load(function(YT) {
+        new YT.Player('player' + videoId, {
+          height: '390',
+          width: '640',
+          videoId: videoId
+        });
+      });
+
+
         return (
                 <div>
                     <h1> {this.props.title} </h1>
+                    <div id={'player' + videoId}></div>
+                    <p>{this.props.description}</p>
+                    <p>{this.props.user}<span>{this.props.views}</span></p>
+                    <p>{this.props.date}</p>
+                    {/* <p>{tagList}</p> */}
+                    
                 </div>
         )
     }
